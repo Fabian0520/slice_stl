afcc935ec2438c58c15e26b880aa5943ea1c7ba6
Fabian Maisberger <fabian.maisberger@googlemail.com>
2020-01-10 11:50:42 +0100

    Ideen zum fitten

--- plane_fit.py
+++ plane_fit.py
@@ -2,9 +2,21 @@
 import slice as sl
 import numpy as np
 import pandas as pd
-cs, mesh, __ = sl.slice_mesh('D-69057.stl')
+cs, mesh, __ = sl.slice_mesh('D-69023.stl')
 points = pd.DataFrame(mesh.vertices, columns=['x','y','z'])
 xy1 = np.concatenate([points.iloc[:,:-1], np.ones((points.shape[0],1))],axis=1)
 z= np.array(points.iloc[:,-1]).reshape(-1,1)
 fit = np.matmul(np.matmul(np.linalg.inv(np.matmul(xy1.T, xy1)), xy1.T), z)
 fit
+
+'''
+trimesh.points.plane_fit fittet plane an Punkte
+trimesh.geometry.plane_transform(ursprung, vector) liefert Transformationsmatrix um Objekt nach Plane auszurichten
+Objekt.apply_transform transformiert das Objekt
+
+1)  plane_fit
+2)  Transformationsmatrix berechnen
+3)  Objekt drehen
+4)  neuer plane_fit mit maskierten Punkten (evlt noch points_plane_distance ansehen)
+    -> 2 bis 4
+5)
