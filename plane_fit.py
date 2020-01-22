b6e51c7b568a722fcbc3e20055687b81a6cb84c0
Fabian Maisberger <fabian.maisberger@googlemail.com>
2020-01-22 10:43:41 +0100

    all important infos are now stored inside the slice dataframe (sphere, crater, ...). Changed the plots.

--- plane_fit.py
+++ plane_fit.py
@@ -5,21 +5,22 @@ import pandas as pd
 
 def prepare_and_fit(mesh_file):
     mesh = trimesh.load_mesh(mesh_file)
-    # Funktoniert mometan nur, wenn kein Pfad angegeben wird.
+    #-----------------------------------------------------------------------------------------------------------------
+    #               Fit Plane
     plane_fit = trimesh.points.plane_fit(mesh.vertices)
     trans_matrix = trimesh.geometry.plane_transform(plane_fit[0], plane_fit[1])
     mesh.apply_transform(trans_matrix)
 
     mesh_points = pd.DataFrame(mesh.vertices, columns=['x', 'y', 'z'])  # convert vertices to pd.DF
-# calculate histogram
+    # calculate histogram
     hist_values, hist_bins = np.histogram(mesh_points['z'], bins=100, density=True)
     # import ipdb; ipdb.set_trace()
-# find sharpest edge in histogram and take this as zero plane
+    # find sharpest edge in histogram and take this as zero plane
     null_pkt = np.array([np.absolute(hist_values[i-1] - hist_values[i]) for i in range(1,len(hist_values))]).argmax()
     # import ipdb; ipdb.set_trace()
-# mask points so they are near hist_max
+    # mask points so they are near hist_max
     mask = mesh_points['z'].between(hist_bins[null_pkt], hist_bins[null_pkt+1])
-# new fit and transform
+    # new fit and transform
     plane_fit = trimesh.points.plane_fit(mesh.vertices[mask])
     trans_matrix = trimesh.geometry.plane_transform(plane_fit[0], plane_fit[1])
     mesh.apply_transform(trans_matrix)
@@ -29,30 +30,26 @@ def prepare_and_fit(mesh_file):
     plane_fit = trimesh.points.plane_fit(mesh.vertices[mask])
     trans_matrix = trimesh.geometry.plane_transform(plane_fit[0], plane_fit[1])
     mesh.apply_transform(trans_matrix)
+    #-----------------------------------------------------------------------------------------------------------------
 
-
-# fit sphere to points not in plane
+    #-----------------------------------------------------------------------------------------------------------------
+    #                                       fit sphere to points not in plane
     mesh_points = pd.DataFrame(mesh.vertices, columns=['x', 'y', 'z'])  # generate new points, after transform
-    mask = mesh_points['z'] < -1.0 # take only points with z<0.5 *** wert noch automatisch eryeugen
+    mask = mesh_points['z'] < -1.0 # take only points with z<-1.0 *** wert noch automatisch eryeugen
     sph_fit_c, sph_fit_r, sph_fit_err = trimesh.nsphere.fit_nsphere(mesh_points[mask])
-
-
-    # import ipdb; ipdb.set_trace()
-# Maske "Abstand Punkte von Kugel kleiner x" erzeugen und noch mal fitten (x10)
+    # Maske "Abstand Punkte von Kugel kleiner x" erzeugen und noch mal fitten (x10)
     for i in range(0,10):
-# Distance between points and sphere surface
+    # Distance between points and sphere surface
         mesh_points['dist_sph'] = np.absolute(np.linalg.norm(mesh_points.loc[:,'x':'z']-sph_fit_c, axis=1.0)-sph_fit_r)
         mask = mesh_points['dist_sph'] < 0.5 #  1.0 - (i/60)
         sph_fit_c, sph_fit_r, sph_fit_err = trimesh.nsphere.fit_nsphere(mesh_points.loc[:,'x':'z'][mask])
     # import ipdb; ipdb.set_trace()
 
-# create and aligning sphere and mesh
+    # aligning sphere and mesh
     trans_matrix_mesh = np.array([[1,0,0,-sph_fit_c[0]],[0,1,0,-sph_fit_c[1]],[0,0,1,0],[0,0,0,1]]) # align mesh and sphere in xy plane, so that origin is colinear with c_sphere
     mesh.apply_transform(trans_matrix_mesh)
     sph_fit_c[:2]=0 # x und y of sphere are 0, since the mesh has been moved
-
-    mesh_points = pd.DataFrame(mesh.vertices, columns=['x', 'y', 'z'])  # generate new points, after transform
-    mesh_points['dist_sph'] = np.absolute(np.linalg.norm(mesh_points.loc[:,'x':'z']-sph_fit_c, axis=1.0)-sph_fit_r)
+    #-----------------------------------------------------------------------------------------------------------------
 
     return mesh, [sph_fit_c, sph_fit_r, sph_fit_err]
 
--- slice.py
+++ slice.py
@@ -7,21 +7,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plane_fit
 
-mesh_file = '/home/fabian/Projekte/Python/vtk/D-68966.stl'
+def slice_mesh(mesh, mesh_name, fit, location=None, direction=[0, -1, 0]):
+    '''
+        Schneidet das Netz mit einer Ebene (Ort und Richtung
+        können definiert werden).
+        mesh: zu schneidendes Netz [trimesh.mesh]
+        mesh_name: Name des Netzes. Wird für den Index der Slices verwendet [str]
+        fit: Parameter der gefitteten Kugel (aus plane_fit.py). Werden auch in ausgabe gespeichert.
+        location: Punkt der Schnittebene [array(3)]
+        direction: Normale der Schnittebene (default: x-z Ebene) [array(3)]
+    '''
 
-def slice_mesh(mesh_file, mesh_name, location=None, direction=[0, -1, 0]):
-    #mesh = trimesh.load_mesh(mesh_file)
-    # Funktoniert mometan nur, wenn kein Pfad angegeben wird.
-    #mesh_name = mesh_file.split('.')[0]
     points = pd.DataFrame(mesh.vertices,
                          columns=pd.MultiIndex.from_product([[mesh_name],
                                                              ['x', 'y', 'z']]))
-    # minimum der z-Spalte:
     if not location:
         # Location auf Minimum der Z-Koordinate
         # .idxmin liefert Zeile des Minimums in der angegebenen Spalte
         location = points.iloc[points[(mesh_name, 'z')].idxmin()]
-    # z-Position = 0, damit der Rand auf 0 Niveau ist
     cross_section = mesh.section(plane_origin=[location[0],
                                             location[1],
                                             0], plane_normal=direction)
@@ -31,9 +34,24 @@ def slice_mesh(mesh_file, mesh_name, location=None, direction=[0, -1, 0]):
     # sort_values(['x']), damit beim lineplot die Punkte richtig liegen.
     cross_section_df = pd.DataFrame(cross_section.vertices,
                                  columns=columns).sort_values(by=[(mesh_name, 'x')]).reset_index(drop=True)
+    r_crater = np.sqrt(fit[1]**2 - fit[0][2]**2)    # calculate crater radius
+    cross_section_df[(mesh_name, 'min_z')] = pd.Series(points.iloc[points[(mesh_name, 'z')].idxmin()].values)
+    cross_section_df[(mesh_name, 'max_z')] = pd.Series(points.iloc[points[(mesh_name, 'z')].idxmax()].values)
+    cross_section_df[(mesh_name, 'sph_center')] = pd.Series(fit[0])
+    cross_section_df[(mesh_name, 'sph_radius')] = pd.Series(fit[1])
+    cross_section_df[(mesh_name, 'sph_error')] = pd.Series(fit[2])
+    cross_section_df[(mesh_name, 'r_crater')] = pd.Series(r_crater)
     return cross_section_df, location
 
 def plot_slices(data, aspect_ratio=1):
+    '''
+        Plottet die Schnitte durch die Netze. fig2 sind die Einzelplots, fig sind
+        die Übersihtsplots (falls mehrer Slices übergeben werden).
+
+        data: zu plottende Date. [pd.DataFrame]
+        aspect_ratio: Aspect ratio der Axen
+    '''
+
     fig, ax = plt.subplots(figsize=(11.6929, 8.26772))   # Din A4 Größe in inch Landscape
     ax.set_aspect(aspect=aspect_ratio)
     ax.set_title('Schnitte durch Zentrum Krater')
@@ -54,8 +72,12 @@ def plot_slices(data, aspect_ratio=1):
         ax2.set_ylim(-35,15)
         ax2.set_xlabel('X [mm]')
         ax2.scatter(data.xs('x',level=1,axis=1), data.xs('z',level=1,axis=1), color='gray', s=0.1, alpha=0.4) # select in a multiindex df in the second level
+        min_z_sph = data[(name, 'sph_center')][2] - data[(name, 'sph_radius')][0]
+        min_z = data[(name, 'min_z')][2]
+        label = (name + '\n' +
+                 'min_z = {0:6.2f} mm \nmin_z_sph = {1:6.2f} mm'.format(min_z, min_z_sph))
         ax2.scatter(data[(name, 'x')], data[(name, 'z')],
-                   label=(name + '\n'r'min_z = '+'{:6.2f} mm'.format(data[(name, 'z')].min())), s=0.5)
+                   label=label, s=0.5)
         ax2.add_artist(plot_circle(0,data[(name,'sph_center')][2],data[(name,'sph_radius')][0]))
         ax2.legend(markerscale=6,
                   scatterpoints=1,
@@ -68,8 +90,12 @@ def plot_slices(data, aspect_ratio=1):
         fig2.savefig(output_name, orientation='landscape', papertype='a4', dpi=600)
         plt.close(fig2)
         #----------------------------------------------------------------------------------------
+        min_z_sph = data[(name, 'sph_center')][2] - data[(name, 'sph_radius')][0]
+        min_z = data[(name, 'min_z')][2]
+        label = (name + '\n' +
+                 'min_z = {0:6.2f} mm \nmin_z_sph = {1:6.2f} mm'.format(min_z, min_z_sph))
         ax.scatter(data[(name, 'x')], data[(name, 'z')],
-                   label=(name + '\n'r'min_z = '+'{:6.2f} mm'.format(data[(name, 'z')].min())), s=0.5)
+                   label=label, s=0.5)
         # ax.add_artist(plot_circle(0,data[(name,'sph_center')][2],data[(name,'sph_radius')][0]))
     ax.legend(markerscale=6,
               scatterpoints=1,
@@ -82,6 +108,10 @@ def plot_slices(data, aspect_ratio=1):
     return fig, ax
 
 def plot_circle(x,y, radius=0.15):
+    '''
+        Erzeugt die Kreise für plot_slices().
+    '''
+
     from matplotlib.patches import Circle
     from matplotlib.patheffects import withStroke
     circle = Circle((x,y), radius, clip_on=True, zorder=10, linewidth=1,
@@ -97,6 +127,8 @@ def read_slices():
         data = pd.concat([data, pd.read_json(file)], axis=1, sort=False)
     return data
 
+#data = pd.read_csv('file', header=[0,1])
+
 if __name__ == '__main__':
     files = glob.glob('*.stl')
     print(files)
@@ -105,15 +137,8 @@ if __name__ == '__main__':
         mesh_name = file.split('.')[0]
         output_name = mesh_name + '.csv'
         mesh, sphere_fit_parameters = plane_fit.prepare_and_fit(file)
-        r_crater = np.sqrt(sphere_fit_parameters[1]**2 - sphere_fit_parameters[0][2]**2)
-        print('Name: {0}, tiefster Punkt (Kugel): {1:4.2f}mm, Durchmesser Krater: {2:4.2f}mm'.format(mesh_name, sphere_fit_parameters[0][2]-sphere_fit_parameters[1],r_crater*2))
-        cs, __ = slice_mesh(mesh, mesh_name, location=[0,0])
-        #import ipdb; ipdb.set_trace()
-        cs[mesh_name, 'sph_center'] = pd.Series(sphere_fit_parameters[0])
-        cs[mesh_name, 'sph_radius'] = pd.Series(sphere_fit_parameters[1])
-        cs[mesh_name, 'sph_error'] = pd.Series(sphere_fit_parameters[2])
+        cs, __ = slice_mesh(mesh, mesh_name, sphere_fit_parameters, location=[0,0])
         cs.to_csv(output_name, index=False)
         # führt alle Schnitte in einem Dataframe zusammen, damit dann alle geplottet werden können
         cross_sections = pd.concat([cross_sections, cs], axis=1)
-        # cross_sections.to_csv('tmp.csv', index=False)
     plot_slices(cross_sections)
